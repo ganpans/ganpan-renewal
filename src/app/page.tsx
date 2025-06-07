b3bbea9dd0e04b3364d712d27fa97299270ec6ca
KwonET <dlxl3652zz@gmail.com>
2025-06-07 10:29:16 +0900

    style: letter spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { Body, Caption, H1 } from '../components/ui/Typography';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        테스트
+        <H1 className="tracking-kr">한국어입니다</H1>
+        <H1 className="tracking-en">English</H1>
+        <H1 className="tracking-kr">
+          한국어와 <span className="tracking-en">English Mix</span>
+        </H1>
+        <section className="[&_*]:tracking-kr">
+          <H1>한국어 제목</H1>
+          <Body>한국어 본문</Body>
+          <Caption>한국어 캡션</Caption>
+        </section>
+        <section className="[&_*]:tracking-en">
+          <H1>English Title</H1>
+          <Body>English Body</Body>
+          <Caption>English Caption</Caption>
+        </section>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,18 +16,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: {
-          50: '#eff6ff',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-        },
-        secondary: {
-          50: '#f0fdf4',
-          500: '#22c55e',
-          600: '#16a34a',
-          700: '#15803d',
-        },
+        primary: '#FF00FA',
       },
       spacing: {
         18: '4.5rem',
