6a8230825d55a3d0041d9e3643ada1c31d1eb8d3
KwonET <dlxl3652zz@gmail.com>
2025-06-07 13:57:14 +0900

    feat: svg package

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
+import Ganpan from '../../../public/ganpan.svg';
 import Progress from '../ui/Progress';
 import Toggle from '../ui/Toggle';
-
 const Header = ({ step }) => {
   return (
     <div className="flex justify-between">
-      <Toggle />
+      <div className="flex gap-8">
+        <Ganpan width={225} height={62} />
+        <Toggle />
+      </div>
       <Progress step={step} />
     </div>
   );
